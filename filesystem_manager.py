3a9f9c2bb7ec88adc5ddb09f41f138d8fd3ea427
David Lin <davidlinzijian@gmail.com>
2025-05-07 19:59:07 -0400

    add retry for item details

--- filesystem_manager.py
+++ filesystem_manager.py
@@ -2,4 +2,4 @@ from pathlib import Path
 from item import Item
 
 def create_dir_for_item(item: Item):
-	Path("training/" + str(item.set_id) + "/" + item.id).mkdir(parents=True, exist_ok=True)
+	Path("training/" + item.id).mkdir(parents=True, exist_ok=True)
--- main.py
+++ main.py
@@ -19,9 +19,13 @@ def setup_training_directories():
 	for item in get_items_from_db():
 		create_dir_for_item(item)
 
-setup_database()
 items_details = []
 for item in tqdm(get_items_from_db(), desc="fetching item details", unit="item"):
-	items_details.append(get_item_details_from_item(item))
+	for i in range(10):
+		try:
+			items_details.append(get_item_details_from_item(item))
+			break
+		except:
+			print('try ' + str(i))
 
 insert_item_details(items_details)
