2ec41d5e9e99b5662e96e244599ebc494ad5ec12
David Lin <davidlinzijian@gmail.com>
2025-05-14 11:27:46 -0400

    save model weights after training and change epoch to 20

--- vision_trainer.py
+++ vision_trainer.py
@@ -12,10 +12,10 @@ from tqdm import tqdm
 num_classes = 55865
 image_size = 224
 batch_size = 32
-epochs = 10
+epochs = 20
 learning_rate = 1e-4
 eval_percentage = 0.1
-num_workers = 4
+num_workers = 1
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def main():
@@ -34,10 +34,11 @@ def main():
     train_size = total_size - eval_size
 
     # === Split the dataset into training and validation ===
-    train_dataset, val_dataset = random_split(full_dataset, [train_size, eval_size])
+    train_dataset = full_dataset
+    # train_dataset, val_dataset = random_split(full_dataset, [train_size, eval_size])
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers, pin_memory=True)
+    # val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers, pin_memory=True)
 
     torch.set_float32_matmul_precision('high')
 
@@ -71,20 +72,20 @@ def main():
         acc = 100 * correct / total
         print(f"Epoch {epoch+1} - Loss: {running_loss:.4f} - Accuracy: {acc:.2f}%")
 
-        # torch.save(model.state_dict(), 'trained_model.pth')
+    torch.save(model.state_dict(), 'trained_model.pth')
 
-    # === Validation Accuracy ===
-    model.eval()
-    val_correct, val_total = 0, 0
-    with torch.no_grad():
-        for imgs, labels in val_loader:
-            imgs, labels = imgs.to(device), labels.to(device)
-            outputs = model(imgs)
-            _, preds = torch.max(outputs, 1)
-            val_correct += (preds == labels).sum().item()
-            val_total += labels.size(0)
+    # # === Validation Accuracy ===
+    # model.eval()
+    # val_correct, val_total = 0, 0
+    # with torch.no_grad():
+    #     for imgs, labels in val_loader:
+    #         imgs, labels = imgs.to(device), labels.to(device)
+    #         outputs = model(imgs)
+    #         _, preds = torch.max(outputs, 1)
+    #         val_correct += (preds == labels).sum().item()
+    #         val_total += labels.size(0)
 
-    print(f"Validation Accuracy: {100 * val_correct / val_total:.2f}%")
+    # print(f"Validation Accuracy: {100 * val_correct / val_total:.2f}%")
 
 if __name__ == "__main__":
     main()
